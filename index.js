5d8567d077623f1b3414bc7f615f984f43f2b92c
Xin-Xin Wang - blueberry <xin-xinw@musiclifephilosophy.com>
2016-03-17 20:35:17 -0400

    Set up server files

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static('public'));
+app.use('/lib', express.static('lib'));
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+http.listen(8080, function(){
+  console.log('listening on *:8080');
+});
