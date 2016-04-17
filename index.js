b9bf857370c70cf89aff741b74cf7106e9b832b9
Xin-Xin Wang - blueberry <xin-xinw@musiclifephilosophy.com>
2016-04-17 13:31:28 -0400

    move life-switch to life-tools, mem

--- index.js
+++ index.js
@@ -10,10 +10,6 @@ app.use(express.static('lib/life-client'));
 var $ = require('./lib/tools/tools');
 var LS = require('./lib/life-tools/life-state');
 
-var nsp = io.of("/states");
-
-var rooms = {};
-
 var udfp = $.udfp;
 var min = $.min;
 
@@ -26,50 +22,51 @@ var MAX_REFSPEED = 10;
 var MAX_SIZE_ROWS = 100;
 var MAX_SIZE_COLS = 200;
 
+var states = io.of("/states");
+
 function makeRoom(room){
-  if (udfp(room))room = "global";
   console.log("making room", room);
   
   var state = LS.makeLifeState(80, 170);
   
   state.onstart = function (){
     console.log(room, 'state start');
-    nsp.to(room).emit('start');
+    states.to(room).emit('start');
   };
   
   state.onstop = function (){
     console.log(room, 'state stop');
-    nsp.to(room).emit('stop');
+    states.to(room).emit('stop');
   };
   
   state.onset = function (st, i, j){
     console.log(room, 'send set');
-    nsp.to(room).emit('set', st, i, j);
+    states.to(room).emit('set', st, i, j);
   };
   
   state.onsetobj = function (st, i, j, obj){
     console.log(room, 'send setobj');
-    nsp.to(room).emit('setobj', st, i, j, obj);
+    states.to(room).emit('setobj', st, i, j, obj);
   };
   
   state.onsetstate = function (newstate){
     console.log(room, 'send setstate');
-    nsp.to(room).emit('setstate', newstate);
+    states.to(room).emit('setstate', newstate);
   };
   
   state.onspeed = function (s){
     console.log(room, 'send speed');
-    nsp.to(room).emit('speed', s);
+    states.to(room).emit('speed', s);
   };
   
   state.onrefspeed = function (r){
     console.log(room, 'send refspeed');
-    nsp.to(room).emit('refspeed', r);
+    states.to(room).emit('refspeed', r);
   };
   
   state.onsize = function (r, c){
     console.log(room, 'send size');
-    nsp.to(room).emit('size', r, c);
+    states.to(room).emit('size', r, c);
   };
   
   function addUser(socket){
@@ -156,19 +153,25 @@ function makeRoom(room){
   };
 }
 
-function getRoom(room){
-  if (udfp(room))room = "global";
-  console.log("getting room", room);
-  if (!udfp(rooms[room])){
-    console.log("room", room, "already exists");
-    return rooms[room];
+function mem(make, defKey){
+  if (udfp(defKey))defKey = '';
+  
+  var m = {};
+  
+  function get(a){
+    if (udfp(a))a = defkey;
+    if (!udfp(m[a]))return m[a];
+    return m[a] = make(a);
   }
-  var r = makeRoom(room);
-  rooms[room] = r;
-  return r;
+  
+  return {
+    get: get
+  };
 }
 
-nsp.on('connection', function (socket){
+var rooms = mem(makeRoom, 'global');
+
+states.on('connection', function (socket){
   console.log('a user connected to states');
   
   socket.on('disconnect', function (){
@@ -178,7 +181,7 @@ nsp.on('connection', function (socket){
   socket.on('join', function (room){
     if (!strp(room))return;
     console.log('join', room);
-    getRoom(room).addUser(socket);
+    rooms.get(room).addUser(socket);
   });
 });
 
